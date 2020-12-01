c5a42f373b95bb502b99f4921bd7e9982e60f1eb
lusitdev <lusitdev@gmail.com>
2020-12-01 20:17:44 +0100

    Removed useless class 'mes'

--- chrome/src/popup.js
+++ chrome/src/popup.js
@@ -25,7 +25,6 @@ const render = (() => {
     init: function () {
       const vars = ['header', '#result', '#loading', '#result button'];
       const arr = vars.map(ele => document.querySelector(ele));
-
       [dom.header, dom.result, dom.loading, dom.button] = arr;
       dom.buttonSpan = arr[3].querySelector('span');
       [dom.initHeader, dom.initResult] = arr.slice(0, 2).map(e => e.cloneNode(true)); // Clone the initial state of output elements
@@ -54,7 +53,7 @@ const render = (() => {
   function blurHandler(event) {
     let data = event.target.innerText;
 
-    data = (data === `\n`) ? '' : data;
+    data = (data === `\n`) ? '' : data; 
     if (data !== sellers) {
       sellers = data;
       background.saveSync({
@@ -86,7 +85,7 @@ const render = (() => {
     icon.textContent = ico;
     return icon;
   }
-
+  
   function addRefreshButton() {
     const refresh = document.createElement('button');
     const domain = msg.domain.includes('www.') ? msg.domain.slice(4) : msg.domain;
@@ -97,7 +96,7 @@ const render = (() => {
     dom.header.append(refresh);
     refresh.addEventListener('click',
       function () {
-        dom.reInit(msg.status, copyMissing).then(background.refetch());
+        dom.reInit(msg.status, copyMissing).then(background.refetch()); 
       }, {
         once: true
       }
@@ -147,22 +146,22 @@ const render = (() => {
     }
 
     let sellerMiss;
-
     if (msg.check) {
       sellerMiss = msg.check[0] + ' unauthorized seller';
       sellerMiss += (msg.check[0] > 1) ? 's' : '';
     }
 
     const resTextColor = [
-      ['n / a', 'mes'], // Status 0 - Tab content not a website
-      ['ads.txt not found', 'mes'], // Status 1 - No seller restrictions
-      ['No sellers specified', 'mes'], // Status 2 - No sellers to test 
+      ['n / a'], // Status 0 - Tab content not a website
+      ['ads.txt not found'], // Status 1 - No seller restrictions
+      ['No sellers specified'], // Status 2 - No sellers to test 
       [sellerMiss, 'unau'], // Status 3 - Found missing sellers
       ['All sellers authorized!', 'auth'] // Status 4 - All sellers OK
     ];
 
     if (messageIndex < 2) {
       dom.loading.hidden = true; // Hide loading spinner
+
       if (msg.status > 0) {
         addRefreshButton();
         if (msg.status == 1 && msg.content.includes('content-type')) {
@@ -197,7 +196,9 @@ const render = (() => {
         },
       )
     },
+
   }
 })();
 
 chrome.runtime.getBackgroundPage(render.handleBg);
+
